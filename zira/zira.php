5eecd711b38a400c7dd63b3fe03762bfdfca9b54
Dro1d.Ru <ziracms@gmail.com>
2017-03-09 15:23:17 +0500

    ver. 0.5.0

--- zira/zira.php
+++ zira/zira.php
@@ -10,7 +10,7 @@ namespace Zira;
 use Dash\Dash;
 
 class Zira {
-    const VERSION = '0.4.1';
+    const VERSION = '0.5.0';
     private static $_instance;
 
     public static function getInstance() {
