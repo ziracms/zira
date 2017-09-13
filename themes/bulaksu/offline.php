f2847cf994fb23e2afd00f388e9d7e5ee9de1d8c
Dro1d.Ru <ziracms@gmail.com>
2017-09-13 16:25:52 +0500

    offline page fix for mobile

--- themes/bulaksu/offline.php
+++ themes/bulaksu/offline.php
@@ -1,7 +1,9 @@
+<div class="offline-page-wrapper">
 <div class="alert alert-danger" role="alert"><?php echo t('Site offline'); ?></div>
-<div style="float:left"><img src="<?php echo Zira\Helper::baseUrl('assets/images/smileys/svg/1f916.svg'); ?>" width="300" height="300" alt="<?php echo t('Site offline'); ?>" /></div>
-<div style="margin-left:300px;min-height:300px;padding:100px 0px 0px 30px;">
+<div class="offline-page-image"><img src="<?php echo Zira\Helper::baseUrl('assets/images/smileys/svg/1f916.svg'); ?>" width="300" height="300" alt="<?php echo t('Site offline'); ?>" /></div>
+<div class="offline-page-text">
 <p><?php echo t('Sorry, but website is currently under maintenance.'); ?></p>
 <p><?php echo t('We should be back shortly.'); ?></p>
 <p><?php echo t('Thank you for your patience.'); ?></p>
+</div>
 </div>
