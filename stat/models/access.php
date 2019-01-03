4a9b1326ca4758c0b8b44dfe4fb33c8e2e6c838f
Dro1d.Ru <ziracms@gmail.com>
2019-01-04 02:26:39 +0500

    minor fix

--- stat/models/access.php
+++ stat/models/access.php
@@ -61,6 +61,7 @@ class Access extends Orm {
                     )
                 ){ 
                     $is_bot = true;
+                    $referer = '';
                 }
             }
             
