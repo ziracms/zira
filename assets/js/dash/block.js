2389901fb17973c958f4898907668ad26fab98f8
Dro1d.Ru <ziracms@gmail.com>
2021-01-07 18:42:53 +0500

    themes updated

--- assets/js/dash/block.js
+++ assets/js/dash/block.js
@@ -1,7 +1,7 @@
 var dash_block_load = function() {
     desk_window_form_init(this);
     var ta = $(this.content).find('textarea');
-    $(ta).wrap('<div style="border:1px solid #ccc;border-radius:4px;overflow:hidden;width:100%;height:'+$(ta).height()+'px"></div>');
+    $(ta).wrap('<div class="zira-cm-wrapper" style="height:'+$(ta).height()+'px"></div>');
     this.cm = zira_codemirror($(this.content).find('textarea'));
 };
 
